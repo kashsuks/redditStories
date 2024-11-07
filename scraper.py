bd68b78472463c5f523ce8d379dd16911e8c5b5a
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-11-07 18:54:10 -0500

    added accent

--- scraper.py
+++ scraper.py
@@ -9,15 +9,16 @@ from playsound import playsound
 sampleScript = """This is a sample script, please change this later for the actual script that is
 being pulled from reddit"""
 language = 'en'
+accent = 'us'
 
 # TTS object generation
-myobj = gTTS(text=sampleScript, lang=language, slow=True)
+myobj = gTTS(text=sampleScript, lang=language, slow=True, tld=accent)
 myobj.save("welcome.mp3")
 
 #Cross-platform i like my cheese drippy
 def play_audio(file_path):
     if platform.system() == "Windows":
-        os.system(f"start {file_path}")
+        os.system(f"start {file_path}")     
     elif platform.system() == "Darwin":  # macOS
         subprocess.call(["afplay", file_path])
     elif platform.system() == "Linux":
@@ -25,5 +26,5 @@ def play_audio(file_path):
     else:
         playsound(file_path)
 
-#Play the fucking audio
+#Play the audio
 play_audio("welcome.mp3")
